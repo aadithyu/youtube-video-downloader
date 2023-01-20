cd599eb63021080966db0fb814136ed5a437a276
aadithyu <83775103+aadithyu@users.noreply.github.com>
2023-01-20 17:21:22 +0530

    Create GUI yt downloader.py

---
+++ GUI yt downloader.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from pytube import YouTube
+
+root = Tk()
+root.geometry('450x300')
+
+root.title("Youtube Video Downloader")  
+
+Label(root, text='Youtube Video Downloader',
+      font='arial 20 bold').pack() 
+
+link = StringVar()  
+filename = StringVar()  
+
+Label(root, text='Paste Link Below:', font='arial 13 bold').place(x=160, y=40)
+link_enter = Entry(root, width=57, textvariable=link).place(
+    x=50, y=90)  
+
+def Download():  
+
+    Label(root, text='Downloading..', font='arial 13').place(x=170, y=210)
+
+    url = YouTube(str(link.get()))
+
+    video = url.streams.get_highest_resolution()
+
+    video.download()
+
+    Label(root, text='Downloaded', font='arial 15').place(x=170, y=210)
+
+
+Button(root, text='DOWNLOAD', font='arial 15 bold',
+       padx=4, command=Download).place(x=150, y=150)
+
+root.mainloop()
+
